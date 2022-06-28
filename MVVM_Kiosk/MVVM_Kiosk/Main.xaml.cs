6efa1bee7f2dc7414b102d0f8665646ef206410f
GGoChan <go950910@naver.com>
2022-06-28 22:55:40 +0900

    06_28 commit
    06_28 commit

--- MVVM_Kiosk/MVVM_Kiosk/Main.xaml.cs
+++ MVVM_Kiosk/MVVM_Kiosk/Main.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace MVVM_Kiosk
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// Main.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : Window
     {
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
         }
